92aeb010c34d6e64474ecab4a4233ea861ded04c
Zane <github@zane.sh>
2018-10-03 20:52:39 -0700

    Link Attendee & Event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
+  has_many :attendees
   validates :name, :startDate, :endDate, :location, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :events
+  resources :events do
+    resources :attendees
+  end
 
   root 'pages#index'
 end
