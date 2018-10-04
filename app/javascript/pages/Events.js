6d608091dae9f04e495cbf281d8a6f920f8b5920
Zane <github@zane.sh>
2018-10-03 20:28:02 -0700

    Display message if no events exist

--- app/javascript/pages/Events.js
+++ app/javascript/pages/Events.js
@@ -4,13 +4,17 @@ export default ({ events }) => (
   <div>
     <a href="/">Home</a> › <a href="/events">Events</a>
     <h1>Events</h1>
-    {events.map(event => (
-      <div key={event.id}>
-        <a href={`/events/${event.id}`}>
-          <h1>{event.name}</h1>
-        </a>
-      </div>
-    ))}
+    {events && events.length !== 0 ? (
+      events.map(event => (
+        <div key={event.id}>
+          <a href={`/events/${event.id}`}>
+            <h1>{event.name}</h1>
+          </a>
+        </div>
+      ))
+    ) : (
+      <h2>There are no events yet.</h2>
+    )}
     <a href="/events/new">Add an event</a>
   </div>
 )
