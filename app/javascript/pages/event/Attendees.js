020923ee0613412e24e4e3198df1ba1c2549200a
Zane <github@zane.sh>
2018-10-04 14:50:43 -0700

    Make attendees list bulleted

--- app/javascript/pages/event/Attendees.js
+++ app/javascript/pages/event/Attendees.js
@@ -8,13 +8,15 @@ export default ({ attendees, event }) => (
       Attendees: <b>{event.name}</b>
     </h1>
     {attendees && attendees.length !== 0 ? (
-      attendees.map(attendee => (
-        <div key={attendee.id}>
-          <a href={`/${attendee.id}`}>
-            <h1>{attendee.fname}</h1>
-          </a>
-        </div>
-      ))
+      <ul>
+        {attendees.map(attendee => (
+          <li key={attendee.id}>
+            <a href={`/events/${event.id}/attendees/${attendee.id}`}>
+              {attendee.fname} {attendee.lname}
+            </a>
+          </li>
+        ))}
+      </ul>
     ) : (
       <h2>There are no attendees yet.</h2>
     )}
