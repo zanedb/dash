b4784098d621923b3161cd16dc7620a12b75e105
Zane <github@zane.sh>
2018-10-04 14:51:26 -0700

    Add create, update, & destroy methods for attendees

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,6 +1,6 @@
 class AttendeesController < ApplicationController
-  before_action :set_event, only: %i[index show new edit update destroy]
-
+  before_action :set_event
+  
   def index
     @attendees = @event.attendees
     render react_component: 'Attendees', props: {
@@ -13,9 +13,36 @@ class AttendeesController < ApplicationController
     render react_component: 'AttendeesNew', props: { event: @event.as_json }
   end
 
+  def create
+    @attendee = @event.attendees.new(attendee_params)
+
+    if @attendee.save
+      redirect_to @attendee, notice: 'Attendee was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @attendee.update(attendee_params)
+      redirect_to @attendee, notice: 'Attendee was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @attendee.destroy
+    redirect_to attendees_url, notice: 'Attendee was successfully destroyed.'
+  end
+
   private
 
   def set_event
     @event = Event.find(params[:event_id])
+  end
+
+  def attendee_params
+    params.require(:attendee).permit(:fname, :lname, :email, :note, :extras)
   end
 end
