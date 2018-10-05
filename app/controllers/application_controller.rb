a61e343b1f2c6625836ab73f517a92762ef43a10
Zane <github@zane.sh>
2018-10-04 23:52:07 -0700

    Add permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: %i[name]
+    devise_parameter_sanitizer.permit :account_update, keys: %i[name password current_password]
+  end
 end
