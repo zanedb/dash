1ac3ba26011a3fc81d39bf4c4b7514572a7b3ea9
Zane <github@zane.sh>
2018-10-04 23:04:35 -0700

    Fix migration

--- db/migrate/20181005023938_devise_create_users.rb
+++ db/migrate/20181005023938_devise_create_users.rb
@@ -13,13 +13,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       ## Rememberable
       t.datetime :remember_created_at
+      t.text :name, default: ""
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
