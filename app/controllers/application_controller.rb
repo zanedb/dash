6c255655b033d97325c882e8c1a88be1ea45bd92
Zane <github@zane.sh>
2018-10-05 17:10:18 -0700

    Properly return 404s throughout application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   helper_method :nobody_signed_in?, :current_user_id, :is_my?, :isnt_my?
 
   # Keep the current user id in memory
@@ -51,5 +52,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: %i[name]
     devise_parameter_sanitizer.permit :account_update, keys: %i[name password current_password]
+  end
+
+  def record_not_found
+    render react_component: 'NotFound', status: 404
   end
 end
