a34fa5b92ca895852e38eaee36af71a5f442f2c7
Zane <github@zane.sh>
2018-10-13 16:09:07 -0700

    Set "from" field on emails

--- app/mailers/organizer_position_invites_mailer.rb
+++ app/mailers/organizer_position_invites_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrganizerPositionInvitesMailer < ApplicationMailer
-  default from: 'invitations@h-m.zane.sh'
+  default from: 'H&M Invitations <invitations@h-m.zane.sh>'
 
   def notify
     @invite = params[:invite]
