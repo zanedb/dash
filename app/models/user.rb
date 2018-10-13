b8ebc83714510cd7b5489ffa6226c3e43bf70cee
Zane <github@zane.sh>
2018-10-13 00:51:28 -0700

    Add :invitable to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Others available are:
   # :confirmable, :lockable, :timeoutable, and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
   validates :name, presence: true
