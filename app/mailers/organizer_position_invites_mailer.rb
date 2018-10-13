909e8ac2e921aafc28c0e0a06fc722eea25e9152
Zane <github@zane.sh>
2018-10-13 15:53:19 -0700

    Change subject if user is new

--- app/mailers/organizer_position_invites_mailer.rb
+++ app/mailers/organizer_position_invites_mailer.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class OrganizerPositionInvitesMailer < ApplicationMailer
   default from: 'invitations@h-m.zane.sh'
 
   def notify
     @invite = params[:invite]
-    mail(to: @invite.email, subject: 'Welcome to H&M')
+    mail(to: @invite.email, subject: @invite.invite_url ? 'Welcome to H&M' : 'You’ve been invited to manage an event on H&M')
   end
 end
