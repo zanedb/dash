961ac9ab7fa17eea19241cee0758218d55c38a3a
Zane <github@zane.sh>
2018-10-13 00:29:15 -0700

    Fix Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,13 @@
 class Event < ApplicationRecord
   extend FriendlyId
-  belongs_to :user, touch: true
+
+  default_scope { order(id: :asc) }
+
   has_many :attendees, dependent: :destroy
+  has_many :organizer_positions
+  has_many :organizer_position_invites
+  has_many :users, through: :organizer_positions
+  
   validates :name, :startDate, :endDate, :location, :user_id, presence: true
 
   friendly_id :slug_candidates, use: :slugged
