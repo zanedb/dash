502613c32dc7f4966da4f8caa32d5d09875f0b9f
Zane <github@zane.sh>
2018-10-16 21:47:11 -0700

    Use new CSS on all views

--- app/javascript/pages/NotFound.js
+++ app/javascript/pages/NotFound.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default () => (
   <div>
     <h1>404</h1>
-    <h2>This page doesn’t exist, or you don’t have permission to view it.</h2>
+    <h3>This page doesn’t exist, or you don’t have permission to view it.</h3>
   </div>
 )
