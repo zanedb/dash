24344795fa98026a6ce9898e341b47ec56b06e94
Zane <github@zane.sh>
2018-11-11 11:05:58 -0800

    Validate attendee field

--- app/models/attendee_field.rb
+++ app/models/attendee_field.rb
@@ -1,6 +1,19 @@
 class AttendeeField < ApplicationRecord
   belongs_to :event
-  has_many :values, class_name: 'AttendeeFieldValue', foreign_key: 'attendee_field_id'
+
+  validates_presence_of :name, :label, :kind
+  validates :name, uniqueness: {
+    scope: :event,
+    message: 'must be unique'
+  }
+  validates :name, uniqueness: { 
+    case_sensitive: false,
+    message: 'must be lowercase'
+  }
+  validates :name, format: {
+    without: /\s/,
+    message: 'cannot contain spaces'
+  }
 
   KINDS = %i[text multiline checkbox]
 
