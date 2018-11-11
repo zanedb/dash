6fa725ab8553a71dfb6af05755f90ab9439388f7
Zane <github@zane.sh>
2018-11-11 09:34:45 -0800

    Add custom_field helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
-  def format_time time
+  def format_time(time)
     local_time(time, '%B %e, %Y %l:%M%P %Z')
   end
 
@@ -10,5 +12,21 @@ module ApplicationHelper
   def avatar_url(email, size)
     hex = Digest::MD5.hexdigest(email.downcase.strip)
     "https://gravatar.com/avatar/#{hex}?s=#{size}&d=mp"
+  end
+
+  def custom_field(field, form = nil, value = nil)
+    if form && field && value
+      if field.kind == 'text'
+        form.text_field field.name, value: value
+      elsif field.kind == 'multiline'
+        form.text_area field.name, value: value
+      end
+    elsif field
+      if field.kind == 'text'
+        text_field_tag(nil)
+      elsif field.kind == 'multiline'
+        text_area_tag(nil)
+      end
+    end
   end
 end
