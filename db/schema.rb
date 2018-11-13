8866e822913f25ffea63f3abe48ba822c0551289
Zane <github@zane.sh>
2018-11-12 20:06:44 -0800

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_28_061650) do
+ActiveRecord::Schema.define(version: 2018_11_13_040602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_10_28_061650) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "slug"
+    t.string "permitted_domains"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
