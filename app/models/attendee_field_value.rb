f00aa8be5c46b1a7486ff977098ffeb9f877d7d1
Zane <github@zane.sh>
2018-12-01 01:23:34 -0800

    Remove non-working validation

--- app/models/attendee_field_value.rb
+++ app/models/attendee_field_value.rb
@@ -1,8 +1,4 @@
 class AttendeeFieldValue < ApplicationRecord
   belongs_to :attendee, foreign_key: 'attendee_id'
   belongs_to :field, foreign_key: 'attendee_field_id', class_name: 'AttendeeField'
-
-  validates :attendee, uniqueness: {
-    scope: :attendee, message: 'only one field value per attendee'
-  }
 end
