d6239af08d14d7147fa8254eed613ff75fbcbfde
Zane <github@zane.sh>
2018-12-31 14:06:01 -0800

    Move events to root page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   def index
+    if user_signed_in?
+      @events = if current_user.admin?
+                  Event.all
+                else
+                  current_user.events
+                end
+      @invites = current_user.organizer_position_invites.pending
+    end
   end
 end
