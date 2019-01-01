70fc63b4aa62342ed49d745a6c9eded51f62ba50
Zane <github@zane.sh>
2018-12-31 23:53:49 -0800

    Add dropdown to navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,9 +6,36 @@
 //= require jquery3
 
 $(document).ready(function() {
+  // close flash message on click
   $(document).on('click', '[data-behavior~=flash_close]', function() {
     $(this)
       .parent()
       .fadeOut('fast')
+  })
+  // dropdown code
+  $(document).on('click', '[data-behavior~=dropdown_trigger]', function() {
+    // toggle dropdown on profile icon click
+    $('.dropdown__content').toggle()
+    // toggle aria-expanded attribute
+    $('.dropdown__btn').attr('aria-expanded') === 'true'
+      ? $('.dropdown__btn').attr('aria-expanded', 'false')
+      : $('.dropdown__btn').attr('aria-expanded', 'true')
+  })
+  $(document).on('click', e => {
+    // close dropdown on click outside of dropdown
+    if ($(e.target).parent()[0] !== $('.dropdown__btn')[0]) {
+      $('.dropdown__content').hide()
+      $('.dropdown__btn').attr('aria-expanded', 'false')
+    }
+  })
+  $(document).keydown(e => {
+    // close dropdown on esc key press
+    if (
+      e.keyCode === 27 &&
+      $('.dropdown__btn').attr('aria-expanded') === 'true'
+    ) {
+      $('.dropdown__content').hide()
+      $('.dropdown__btn').attr('aria-expanded', 'false')
+    }
   })
 })
