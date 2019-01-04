43bbbd2d216240452aa09106c8489bcdd81549f9
Zane <github@zane.sh>
2019-01-03 20:19:15 -0800

    Fix issue

--- app/controllers/organizer_position_invites_controller.rb
+++ app/controllers/organizer_position_invites_controller.rb
@@ -6,8 +6,6 @@ class OrganizerPositionInvitesController < ApplicationController
     @invite = OrganizerPositionInvite.new
     @invite.event = @event
     authorize @invite
-
-    @invites = @event.organizer_position_invites
   end
 
   def create
