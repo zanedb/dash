97a016659d0fc7e7f9b2b98bbb7f80ff9fcd4091
Zane Davis-Barrs <github@zane.sh>
2019-02-16 00:51:47 -0800

    Minor fixes

--- app/javascript/pages/Fields.js
+++ app/javascript/pages/Fields.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const Field = ({ field, disabled }) => (
   <div className="flex items-end col-12 mb2">
     <div className="flex-auto">
-      <label for={field.name} className="flex items-center">
+      <label htmlFor={field.name} className="flex items-center">
         <strong className="bold">{field.label}</strong>
         <code className="ml1">{field.name}</code>
         {disabled && <span className="badge ml-auto">Default</span>}
@@ -16,37 +16,6 @@ const Field = ({ field, disabled }) => (
   </div>
 )
 
-const DefaultFields = () => (
-  <Fragment>
-    {[
-      { label: 'First Name', name: 'first_name', kind: 'text' },
-      { label: 'Last Name', name: 'last_name', kind: 'text' },
-      { label: 'Email Address', name: 'email', kind: 'email' },
-      { label: 'Note', name: 'note', kind: 'multiline' }
-    ].map(defaultField => (
-      <Field field={defaultField} key={defaultField.name} disabled />
-    ))}
-  </Fragment>
-)
-
-const EditButton = () => (
-  <a className="btn btn--icon ml2">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34" />
-      <polygon points="18 2 22 6 12 16 8 16 8 12 18 2" />
-    </svg>
-  </a>
-)
 const FieldInput = ({ name, kind, options }) => {
   switch (kind) {
     case 'text':
@@ -60,7 +29,15 @@ const FieldInput = ({ name, kind, options }) => {
         />
       )
     case 'multiline':
-      return <textarea placeholder="Multi-line input" id={name} readOnly />
+      return (
+        <Input
+          as="textarea"
+          placeholder="Multi-line input"
+          icon="/icons/multiline.svg"
+          id={name}
+          readOnly
+        />
+      )
     case 'email':
       return (
         <Input
@@ -95,6 +72,37 @@ const Input = styled.input`
   background-repeat: no-repeat;
   padding-left: 2rem !important;
 `
+
+const DefaultFields = () => (
+  <Fragment>
+    {[
+      { label: 'First Name', name: 'first_name', kind: 'text' },
+      { label: 'Last Name', name: 'last_name', kind: 'text' },
+      { label: 'Email Address', name: 'email', kind: 'email' },
+      { label: 'Note', name: 'note', kind: 'multiline' }
+    ].map(defaultField => (
+      <Field field={defaultField} key={defaultField.name} disabled />
+    ))}
+  </Fragment>
+)
+const EditButton = () => (
+  <a className="btn btn--icon ml2">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34" />
+      <polygon points="18 2 22 6 12 16 8 16 8 12 18 2" />
+    </svg>
+  </a>
+)
 
 export default class Fields extends React.Component {
   state = {
