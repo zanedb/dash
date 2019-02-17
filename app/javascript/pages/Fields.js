7783ec6d80d2044957e19c7bf3102b3d40cff033
Zane Davis-Barrs <github@zane.sh>
2019-02-17 01:51:05 -0800

    Fields page is almost done! SO MUCH WORK here Just needs to actually update backend now :)

--- app/javascript/pages/Fields.js
+++ app/javascript/pages/Fields.js
@@ -1,32 +1,82 @@
 import React, { Fragment } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
+import { sortBy } from 'lodash'
 
 class Field extends React.Component {
-  state = {}
+  state = {
+    name: this.props.field.name,
+    label: this.props.field.label,
+    kind: this.props.field.kind,
+    options: this.props.field.options
+  }
+
+  updateLabel = e => {
+    const label = e.target.value
+    const name = label
+      .trim()
+      .replace(/[^\w\s]/gi, '')
+      .toLowerCase()
+      .replace(/ /g, '_')
+    this.setState({ label, name })
+  }
+
+  addOption = () => {
+    const { options } = this.state
+    if (options) {
+      let position = options[options.length - 1].position
+      while (options.some(e => e.position === position)) position++
+
+      this.setState(state => ({
+        options: state.options.concat([{ position, value: '' }])
+      }))
+    } else {
+      this.setState({ options: [{ position: 1, value: '' }] })
+    }
+  }
+  updateOption = (position, value) => {
+    let { options } = this.state
+    const option = options.find(o => o.position === position)
+    const newOption = option
+    newOption.value = value
+    options[options.indexOf(option)] = newOption
+    this.setState({ options })
+  }
 
   render() {
-    const { field, disabled, focused, setFocused } = this.props
+    const { field, disabled, focused, setFocused, deleteField } = this.props
+    const { name, label, kind, options } = this.state
     return (
-      <FieldWrapper focused={focused} onClick={() => setFocused(field.name)}>
+      <FieldWrapper focused={focused}>
         <div className="flex-auto">
           <label
-            htmlFor={field.name}
+            htmlFor={name}
             className="flex items-center"
-            style={{ marginBottom: '2px', pointerEvents: 'none' }}
+            style={{ marginBottom: focused ? '0.5rem' : '2px' }}
           >
-            <strong className="bold">{field.label}</strong>
-            <code className="ml1">{field.name}</code>
+            {focused ? (
+              <Fragment>
+                <FieldName
+                  placeholder="Field name"
+                  value={label}
+                  onChange={this.updateLabel}
+                />
+                <code className="ml1">{name === '' ? '…' : name}</code>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <strong className="bold">{label}</strong>
+                <code className="ml1">{name}</code>
+              </Fragment>
+            )}
             {disabled ? (
               <span className="badge ml-auto">Default</span>
-            ) : (
-              !focused && (
-                <EditField>
-                  Edit
+            ) : focused ? (
+              <div className="ml-auto flex items-center">
+                <Action onClick={() => deleteField(field.name)}>
                   <svg
-                    style={{ marginLeft: '0.25rem' }}
                     xmlns="http://www.w3.org/2000/svg"
-                    width="16"
+                    width="24"
                     height="24"
                     viewBox="0 0 24 24"
                     fill="none"
@@ -35,16 +85,109 @@ class Field extends React.Component {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   >
-                    <g>
-                      <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34" />
-                      <polygon points="18 2 22 6 12 16 8 16 8 12 18 2" />
-                    </g>
+                    <polyline points="3 6 5 6 21 6" />
+                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                    <line x1="10" y1="11" x2="10" y2="17" />
+                    <line x1="14" y1="11" x2="14" y2="17" />
                   </svg>
-                </EditField>
-              )
+                </Action>
+                <Action onClick={() => setFocused('')}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </Action>
+              </div>
+            ) : (
+              <Actions onClick={() => setFocused(field.name)}>
+                Edit
+                <svg
+                  style={{ marginLeft: '0.25rem' }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <g>
+                    <path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34" />
+                    <polygon points="18 2 22 6 12 16 8 16 8 12 18 2" />
+                  </g>
+                </svg>
+              </Actions>
             )}
           </label>
-          <FieldInput {...field} />
+          {focused ? (
+            <Fragment>
+              <select
+                value={kind}
+                icon={`/icons/${kind}.svg`}
+                onChange={e => this.setState({ kind: e.target.value })}
+                className="mb1"
+              >
+                <option value="text">Text input</option>
+                <option value="multiline">Multi-line text input</option>
+                <option value="email">Email input</option>
+                {/*<option value="checkbox">Checkbox</option>*/}
+                <option value="multiselect">Multiple choice</option>
+              </select>
+              {kind === 'multiselect' && (
+                <Fragment>
+                  <div className="flex items-center mb1">
+                    <span className="bold">Options</span>
+                    <Actions onClick={() => this.addOption()}>
+                      Add
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <line x1="12" y1="5" x2="12" y2="19" />
+                        <line x1="5" y1="12" x2="19" y2="12" />
+                      </svg>
+                    </Actions>
+                  </div>
+                  {options ? (
+                    sortBy(options, ['position']).map(option => (
+                      <input
+                        type="text"
+                        value={option.value}
+                        key={option.position}
+                        onChange={e =>
+                          this.updateOption(option.position, e.target.value)
+                        }
+                        placeholder="Option text"
+                        style={{ marginBottom: '0.25rem' }}
+                      />
+                    ))
+                  ) : (
+                    <span className="muted">No options found.</span>
+                  )}
+                </Fragment>
+              )}
+            </Fragment>
+          ) : (
+            <FieldInput name={name} kind={kind} options={options} />
+          )}
         </div>
       </FieldWrapper>
     )
@@ -62,12 +205,19 @@ const FieldWrapper = styled.div.attrs({
     box-shadow: rgba(0, 0, 0, 0.2) 0px 8px 32px;
   `}
 `
-const EditField = styled.span.attrs({
-  className: 'ml-auto flex items-center primary'
+const Action = styled.span.attrs({
+  className: 'primary'
 })`
+  cursor: pointer;
   &:hover {
     color: #0045a6;
   }
+`
+const Actions = styled(Action).attrs({
+  className: 'ml-auto flex items-center primary'
+})``
+const FieldName = styled.input.attrs({ type: 'text' })`
+  max-width: 12rem !important;
 `
 
 const FieldInput = ({ name, kind, options }) => {
@@ -105,14 +255,22 @@ const FieldInput = ({ name, kind, options }) => {
       return (
         <select id={name} readOnly>
           <Fragment>
-            <option value="" disabled>
-              Select an option
-            </option>
-            {options.map(option => (
-              <option value={option} key={option}>
-                {option}
+            {options ? (
+              <Fragment>
+                <option value="" disabled>
+                  Select an option
+                </option>
+                {options.map(option => (
+                  <option key={option.position} value={option.value}>
+                    {option.value}
+                  </option>
+                ))}
+              </Fragment>
+            ) : (
+              <option value="" disabled>
+                No options available
               </option>
-            ))}
+            )}
           </Fragment>
         </select>
       )
@@ -146,7 +304,7 @@ export default class Fields extends React.Component {
 
   getFields() {
     const { event } = this.props.props
-    axios.get(`/events/${event.slug}/fields.json`).then(res => {
+    axios.get(`/events/${event.slug}/registration.json`).then(res => {
       this.setState({ fields: res.data, status: 'ready' })
     })
   }
@@ -154,11 +312,54 @@ export default class Fields extends React.Component {
   setFocused = fieldName => {
     this.setState({ focusedField: fieldName })
   }
+  deleteField = fieldName => {
+    const check = confirm(
+      'Are you sure you want to delete this field and all responses? You cannot undo this.'
+    )
+    if (check) {
+      this.setState(state => ({
+        fields: state.fields.filter(e => e.name !== fieldName),
+        focusedField: ''
+      }))
+    }
+  }
+
+  addField = () => {
+    // TODO: use 'position' to handle case of multiple empty fields
+    // search for field.name and change to field.position
+    const { fields } = this.state
+    let label = ['New field', 0]
+    while (
+      fields.some(
+        e =>
+          (e.label === label[0] && label[1] === 0) ||
+          e.label === label.join(' ')
+      )
+    ) {
+      label[1]++
+    }
+
+    const newLabel = label[1] === 0 ? label[0] : label.join(' ')
+    const name = newLabel
+      .trim()
+      .replace(/[^\w\s]/gi, '')
+      .toLowerCase()
+      .replace(/ /g, '_')
+    this.setState(state => ({
+      fields: state.fields.concat([{ name, label: newLabel, kind: 'text' }])
+    }))
+  }
 
   render() {
     const { fields, focusedField } = this.state
     return (
       <Fragment>
+        <h2 className="heading" style={{ marginBottom: '1rem' }}>
+          Edit signup form
+          <span className="btn" onClick={this.addField}>
+            Add field
+          </span>
+        </h2>
         {status === 'loading' ? (
           <p className="muted">Loading…</p>
         ) : (
@@ -182,6 +383,7 @@ export default class Fields extends React.Component {
                 field={field}
                 focused={focusedField === field.name}
                 setFocused={this.setFocused}
+                deleteField={this.deleteField}
                 key={field.name}
               />
             ))}
