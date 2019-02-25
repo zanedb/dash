47646219f204d9ca7476811eb2937bd0bce80b9c
Zane Davis-Barrs <github@zane.sh>
2019-02-24 20:33:10 -0800

    Generate admin account while seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "\n== Configuring database with sample data =="
+
 event = Event.create!(name: 'Hack the Universe', start_date: 2.days.ago, end_date: 1.days.ago, city: 'Hack City, CA')
 
-p "Created event #{event.name}"
+puts "Created event #{event.name}"
 
 100.times do |index|
   event.attendees.create!(first_name: Faker::Name.first_name,
@@ -17,4 +19,15 @@ p "Created event #{event.name}"
                 created_at: Faker::Time.between(2.months.ago, 1.week.ago))
 end
 
-p "Created #{event.attendees.count} attendees for #{event.name}"
+puts "Created #{event.attendees.count} attendees for #{event.name}"
+
+puts "Enter admin email address: "
+email = STDIN.gets.strip.downcase
+
+user = User.invite!(email: email) do |u|
+  u.skip_invitation = true
+end
+user.make_admin!
+
+puts "Created admin user with email #{email}"
+puts "\nIMPORTANT:\n=========================\nClick this link (once the server is up) to finish your account: http://localhost:3000/users/invitation/accept?invitation_token=#{user.raw_invitation_token}\n========================="
