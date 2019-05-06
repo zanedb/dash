ef5dd201c99d0dd1a5e2080fa47171f9917cf75d
Zane Davis-Barrs <github@zane.sh>
2019-05-05 22:09:51 -0700

    If user has 1 event, go there from index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,10 @@ class PagesController < ApplicationController
     if user_signed_in?
       @events = current_user.events
       @invites = current_user.organizer_position_invites.pending
+
+      if @events.size == 1 && @invites.size == 0 && !current_user.admin?
+        redirect_to current_user.events.first
+      end
     else
       redirect_to new_user_session_path
     end
